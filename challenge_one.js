07ccdad8ac138ffee0d3a65168e285493888e096
aggytwes <54293210+aggytwes@users.noreply.github.com>
2019-09-05 18:14:07 +0300

    Add files via upload
    challenge1 of week 2

---
+++ challenge_one.js
@@ -0,0 +1,10 @@
+<script>
+ prompt("please enter your year of birth.");
+ var age=x
+ if (x<18) ;
+ document,write("you are a minor")
+ else if (x>=18<=36)
+ document.write("you are a youth")
+ else document.write("you are an elder")
+
+</script>
